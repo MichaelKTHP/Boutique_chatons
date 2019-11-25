150f54b781e67b5d41026dad8f7cae80f9f41e90
ballifjbe <estelle.ballif@yahoo.com>
2019-11-25 12:36:22 +0100

    Ajout seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+10.times do |x|
+	User.create(
+		email: x+"yopmail.com",
+		encrypted_password: "azerty")
+	puts "User #{x}"
+
+	Item.create(
+		title: "Titre de l'item" + x)
+	puts "Item #{x}"
+end
+
+3.times do |x|
+	Cart.create(
+       		 user_id: rand(0..10)
+		 item_id: rand (0..10))
+	puts "Cart #{x}"
+
+	Order.create(
+		cart_id: rand(0..3))
+	puts "Order #{x}"
+end
+
